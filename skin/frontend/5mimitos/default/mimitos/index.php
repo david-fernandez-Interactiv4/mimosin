25882cda308b42608a8410a65a90f5b0cb53eb3f
dfmadrid <david.fdmv@gmail.com>
2013-07-10 23:45:44 +0200

    initial commit

---
+++ skin/frontend/5mimitos/default/mimitos/index.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html lang="es-es" dir="ltr">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8" />
+<meta property="og:title" content="Mimitos. Cuidado del bebé - Puericultura - Juegos infantiles - Cuidado de la madre - Estimulación" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://mimitos.herokuapp.com/" />
+<meta property="og:image" content="http://mimitos.herokuapp.com/images/logos/logofb.jpg" />
+<meta property="og:site_name" content="Mimitos" />
+<meta property="og:description" content="Tienda online. Cuidado del bebé y de la madre. Puericultura. Juegos infantiles. Estimulación" />
+<meta property="fb:app_id" content="" />
+<base href="" />
+<meta name="keywords" content="bebé, 'cuidado del bebé', 'cuidado de la madre', 'tienda online', 'productos para el bebé', 'productos para la madre',
+maternidad, puericultura, 'juegos infantiles', 'juegos para bebés', 'juegos para niños', prenatal, 'regalos para niños', 'regalos para bebés', 'embarazo',
+'bautizo', 'baby shower', baby, 'estimulación para bebés', estimulación, 'cajas regalo'" />
+<meta name="description" content="Mimitos. Tienda online con todo para el bebé y las mamás. Regalos, estimulación, juegos para niños y recién nacidos." />
+<title>Mimitos. Cuidado del bebé - Puericultura - Juegos infantiles - Cuidado de la madre.</title>
+<link href="http://mimitos.herokuapp.com/" rel="canonical" />
+<link href="http://mimitos.herokuapp.com/images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+<link rel="stylesheet" href="css/mimitos.css" type="text/css" />
+<link rel="stylesheet" href="css/responsive.css" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Kreon' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 9]>
+<script src="dist/html5shiv.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="row-fluid loginBar blockSpan">
+			<div class="span3">
+			</div>
+			
+			<div class="span6">
+				<div class="row-fluid">
+					<div class="span2">
+						<ul class="inline socialButtons">
+							<li><a href="facebook.com"><img src="http://placehold.it/20x20" /></a>
+							</li>
+							<li><a href="twitter.com"><img src="http://placehold.it/20x20" /></a>
+							</li>
+							<li><a href="twitter.com"><img src="http://placehold.it/20x20" /></a>
+							</li>
+						</ul>
+					</div>
+					<div class="span10">
+						<ul class="inline loginLinks">
+							<li><a class="orangeLetters" href="login.php">Regístrate</a> | <a class="greyLetters" href="sigin.php">Entrar</a>
+							</li>
+							<li><a class="orangeLetters" href="carrito.php"><img src="http://placehold.it/20x20" />Carrito</a> 
+								<a class="greyLetters" href="index.php?productId=90">(1 producto)</a>
+							</li>
+						</ul>
+					</div>
+				</div> <!-- row-fluid loginBar-->
+			</div> <!-- span6 -->
+			
+			<div class="span3">
+			</div>
+		</div> <!-- row-fluid blockSPan -->
+
+		<div class="row-fluid mainBar blockSpan">
+			<div class="span3">
+			</div>
+
+			<div class="span6">
+				<div class="row-fluid">
+					<div class="span3">
+						<img src="http://placehold.it/160x50" />
+					</div>
+					<div class="span7">
+						<ul class="inline nav mainNav">
+							<li><a href="facebook.com">Nuestras cajas</a></li>
+							<li><a href="twitter.com">eShop</a></li>
+							<li><a href="twitter.com">El Club</a></li>
+							<li><a href="twitter.com">Blog</a></li>
+							<li><a href="twitter.com">Maternidad fácil</a></li>
+						</ul>
+					</div>
+					<div class="span2">
+						<img class="pull-right" src="http://placehold.it/30x30" />
+					</div>
+				</div> <!-- row-fluid mainBar-->
+			</div> <!-- span6 -->
+
+			<div class="span3">
+			</div>
+
+		</div> <!-- row-fluid blockSpan -->
+			
+		<div class="row-fluid subMenuBar blockSpan">
+			<div class="span3">
+			</div>
+			
+			<div class="span6">
+			</div>
+			
+			<div class="span3">
+			</div>
+		</div> <!-- row-fluid blockSpan-->
+		
+		<div class="row-fluid navBar blockSpan">
+			<div class="span3">
+			</div>
+			
+			<div class="span6">
+				<p class="greyLetters">Eshop</p>
+				<div class="cyanTop span3"></div>
+			</div>
+			
+			<div class="span3">
+			</div>
+		</div> <!-- row-fluid blockSpan-->
+			
+		<div class="row-fluid blockSpan">
+			<div class="span3">
+			</div>
+
+			<div class="span6">
+				<section class="row-fluid">
+					<aside class="span3">
+						<header>
+							<h1>¿Te Ayudamos?</h1>
+						</header>
+					
+						<p class="orangeLetters">Estoy buscando</p>
+							<ul class="inline thumbnails">
+								<li class="span6"><a href=""><img class="img-rounded" src="http://placehold.it/100x100" />Para mí</a></li>
+								<li class="span6"><a href=""><img class="img-rounded" src="http://placehold.it/100x100" />Un regalo</a></li>
+							</ul>
+						<p class="orangeLetters">Ocasión special</p>
+							<ul class="unstyled optionsNav">
+								<li><a href=""><img class="img-rounded" src="http://placehold.it/20x20" />Embarazo</a></li>
+								<li><a href=""><img class="img-rounded" src="http://placehold.it/20x20" />Nacimiento</a></li>
+								<li><a href=""><img class="img-rounded" src="http://placehold.it/20x20" />Bautizo</a></li>
+								<li><a href=""><img class="img-rounded" src="http://placehold.it/20x20" />Cumpleaños</a></li>
+								<li><a href=""><img class="img-rounded" src="http://placehold.it/20x20" />Baby shower</a></li>
+							</ul>
+					</aside> 
+				
+					<div class="span9">
+						<div class="row-fluid">
+							<section class="thumbnails">
+								<article class="thumbnail span4">
+									<a href="">
+										<img src="http://placehold.it/230x150" />
+										<header>
+											<h1>Portabebé</h1>
+										</header>
+										<p class="orangePrice">12,95 €</p>
+										<footer>
+											<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+										</footer>	
+									</a>	
+								</article>
+								<article class="thumbnail span4">
+									<a href="">
+										<img src="http://placehold.it/230x150" />
+										<header>
+											<h1>Bibendum Dolor Porta</h1>
+										</header>
+										<p class="orangePrice">12,95 €</p>
+										<footer>
+											<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+										</footer>	
+									</a>
+								</article>
+								<article class="thumbnail span4">
+									<a href="">
+										<img src="http://placehold.it/230x150" />
+										<header>
+											<h1>Egestas Euismod Fusce Malesuada</h1>
+										</header>
+										<p class="orangePrice">12,95 €</p>
+										<footer>
+											<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+										</footer>	
+									</a>	
+								</article>
+								<article class="thumbnail span4">
+									<a href="">
+										<img src="http://placehold.it/230x150" />
+										<header>
+											<h1>Ornare Parturient Magna</h1>
+										</header>
+										<p class="orangePrice">12,95 €</p>
+										<footer>
+											<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+										</footer>	
+									</a>	
+								</article>
+								<article class="thumbnail span4">
+									<a href="">
+										<img src="http://placehold.it/230x150" />
+										<header>
+											<h1>Fringilla</h1>
+										</header>
+										<p class="orangePrice">12,95 €</p>
+										<footer>
+											<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+										</footer>	
+									</a>	
+								</article>
+								<article class="thumbnail span4">
+									<a href="">
+										<img src="http://placehold.it/230x150" />
+										<header>
+											<h1>Venenatis Nibh</h1>
+										</header>
+										<p class="orangePrice">12,95 €</p>
+										<footer>
+											<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+										</footer>	
+									</a>	
+								</article>
+								<article class="thumbnail span4">
+									<a href="">
+										<img src="http://placehold.it/230x150" />
+										<header>
+											<h1>Ridiculus</h1>
+										</header>
+										<p class="orangePrice">12,95 €</p>
+										<footer>
+											<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+										</footer>	
+									</a>	
+								</article>
+							</section>
+						</div> <!-- row-fluid -->
+					</div> <!-- span9 -->
+				</section> <!-- row-fluid -->
+			</div> <!-- span6 -->
+
+			<div class="span3">
+			</div>
+		</div> <!-- row-fluid blockSpan -->
+
+		<div class="row-fluid navBar blockSpan">
+			<div class="span3">
+			</div>
+			<div class="span6">
+			</div>
+			<div class="span3">
+			</div>
+		</div> <!-- row-fluid blockSpan-->
+
+		<div class="row-fluid blockSpan">
+			<div class="span3">
+			</div>
+		
+			<div class="span6">
+				<h3>¿Tienes un regalo que hacer? 5mimitos te personaliza la combinación perfecta</h3>
+				<section class="thumbnails regalos">
+					<article class="thumbnail span4">
+						<a href="">
+							<img src="http://placehold.it/140x140" />
+							<header>
+								<h1>Caja recién nacido</h1>
+							</header>
+							<footer>
+								<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+							</footer>	
+						</a>	
+					</article>
+					<article class="thumbnail span4">
+						<a href="">
+							<img src="http://placehold.it/140x140" />
+							<header>
+								<h1>Caja cumpleaños</h1>
+							</header>
+							<footer>
+								<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+							</footer>	
+						</a>	
+					</article>
+					<article class="thumbnail span4">
+						<a href="">
+							<img src="http://placehold.it/140x140" />
+							<header>
+								<h1>Caja Bautizo</h1>
+							</header>
+							<footer>
+								<span class="btn btn-primary"><img src="http://placehold.it/20x20" />Comprar</span>
+							</footer>	
+						</a>	
+					</article>
+				</section> 				
+			</div> <!-- span6 -->
+		
+			<div class="span3">
+			</div>
+		</div> <!-- row-fluid blockSpan-->
+
+		<div class="row-fluid cyan blockSpan">
+			<div class="span3">
+			</div>
+		
+			<div class="span6 cyanFooter">
+				<ul class="inline">
+					<li class="thumbnail">
+						<img src="http://placehold.it/140x140" />
+					</li>
+					<li class="thumbnail">
+							<img src="http://placehold.it/140x140" />
+					</li>
+					<li class="thumbnail">
+						<img src="http://placehold.it/140x140" />
+					</li>
+				</ul>
+			</div> <!-- span6 -->
+		
+			<div class="span3">
+			</div>
+
+		</div> <!-- row-fluid blockSpan -->
+
+		<div class="row-fluid blockSpan">
+			<div class="span3">
+			</div>
+		
+			<div class="span6">
+				<div class="row-fluid footer">
+					<div class="span7">
+						<ul class="inline small endLinks">
+							<li><a href="">Aviso legal y política de privacidad</a></li>
+							<li><a href="">Contacto</a></li>
+							<li><a href="">Preguntas frecuentes</a></li>
+							<li><a href="">Prensa</a></li>
+							<li><a href="">¿Quién es quien?</a></li>
+						</ul>
+					</div> <!-- span6 -->
+					<div class="span2"-->
+						<p class="greyLetters small">
+					</div> <!-- span2 -->
+					<div class="span2">
+						<ul class="inline endLinks">
+							<li><a href=""><img src="http://placehold.it/30x20" /></a></li>
+							<li><a href=""><img src="http://placehold.it/30x20" /></a></li>
+							<li><a href=""><img src="http://placehold.it/30x20" /></a></li>
+						</ul>	
+					</div> <!-- span2 -->
+					<div class="span1">
+						<img src="http://placehold.it/60x30" />
+					</div> <!-- span1 -->
+		
+				</div> <!-- row-fluid -->
+
+			</div> <!-- span6 -->
+
+			<div class="span3">
+			</div> <!-- span3 -->
+		
+		</div> <!-- row-fluid blockSpan -->	
+	</div> <!-- container-fluid -->	
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>	
+</body>
+</html>
+
